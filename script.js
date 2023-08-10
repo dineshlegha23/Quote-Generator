7f1d4f8dc5eec63e0305bac2eb22acde6e1bfeee
Dinesh <abcd@gmail.com>
2023-08-10 19:33:48 +0530

    Button functionality added

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
+let apiQuotes = [];
+
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
+
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+  console.log(quote);
+}
+
 async function getQuotes() {
   const apiURL = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiURL);
-    const data = await response.json();
-
-    const quote = document.querySelector("#quote");
-    const random = Math.floor(Math.random() * data.length);
-    quote.textContent = data[random].text;
-
-    console.log(data);
+    apiQuotes = await response.json();
+    newQuote();
   } catch (error) {}
 }
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 getQuotes();
